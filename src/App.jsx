2aff9a3a890f9f0f4b429cb91f9f8bfc20db913d
ramen72 <ramen@devsramen.com>
2025-03-09 19:15:42 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/css/style.css'
+import HomePage from './pages/homePage/HomePage'
+
+function App() {
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+    return (
+        <>
+            <Background>ts</Background>
+        </>
+    );
+};
+
+export default Navbar;
+
+const Background = styled.div`
+  background-color: red;
+  height: 50px;
+`;
